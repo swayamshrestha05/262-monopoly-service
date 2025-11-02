6a38444be29d2e397e9352e1cf441d7736e196a5
Swayam Shrestha <156826187+swayamshrestha05@users.noreply.github.com>
2025-11-02 15:25:07 -0500

    Add SSL configuration to database connection

--- src/monopolyService.ts
+++ src/monopolyService.ts
@@ -50,6 +50,7 @@ const db = pgPromise()({
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    ssl: { rejectUnauthorized: false },
 });
 
 // Configure the server and its routes
